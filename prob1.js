abd7160b8ac50446310e935c43339d5c87de3c84
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-03-27 22:52:16 +0600

    problem 2 solved

--- prob1.js
+++
@@ -1,5 +0,0 @@
-var totalTk=prompt("Enter Total Tk:");
-var totalCost=prompt("Enter Total Cost:");
-
-var remainTk=totalTk-totalCost;
-console.log(remainTk)
---
+++ problem_solve.js
@@ -0,0 +1,20 @@
+//Problem Solve
+
+//Problem-1
+
+// var totalTk=prompt("Enter Total Tk:");
+// var totalCost=prompt("Enter Total Cost:");
+
+// var remainTk=totalTk-totalCost;
+// console.log(remainTk)
+
+//Problem-2
+
+var maths=70.67;
+var english=80.45;
+var physics=90.25;
+var chemistry=95.00;
+var biology=88.35;
+
+var average=(maths+english+physics+chemistry+biology)/5;
+console.log( average.toFixed(2) )
