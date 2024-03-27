9ecec49f9cadfefb60e56015881a79935b70f982
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-03-27 16:17:46 +0600

    shorthand

---
+++ shorthand.js
@@ -0,0 +1,19 @@
+//Shorthand_Operator
+
+
+var ahsan=30;
+var jun=6;
+
+
+// ahsan=35;
+
+
+//Its called shorthand_operator
+// ahsan=ahsan+10;
+// jun=jun-5;
+ahsan+=10;
+jun-=5; 
+
+console.log(ahsan);
+console.log(jun);
+
