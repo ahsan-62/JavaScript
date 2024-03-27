d7bb9c47664c71d9e648a9a07baab2047c1b31db
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-03-27 15:21:45 +0600

    Arithmatic Operations

---
+++ operation.js
@@ -0,0 +1,31 @@
+
+
+
+var ahsan=30;
+var jun=6;
+
+
+//Addition_Operation
+var totalAmount=ahsan+jun;
+//Subtraction_Operation
+var subTotal=ahsan-jun;
+//Multiplication_Operation
+
+var mulTotal=ahsan*jun;
+//Division_Operation
+var divTotal=ahsan/jun;
+
+//Modulus_Operation
+var modTotal=ahsan%jun;
+
+// console.log(ahsan);
+// console.log(jun);
+// console.log(ahsan+jun);
+console.log(totalAmount);
+console.log(subTotal);
+console.log(mulTotal);
+console.log(divTotal);
+console.log(modTotal);
+
+
+
