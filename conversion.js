cacfeaa3dae8171938ea889df177d69dba307511
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-03-27 16:37:44 +0600

    ParseInt and ParseFloat

---
+++ conversion.js
@@ -0,0 +1,17 @@
+//  var ahsan='30';
+//  var jun='6';
+
+//  var ahsanjun=ahsan+jun;
+//  console.log(ahsanjun);
+
+
+//String To Number Conversion by parseInt and ParseFloat
+
+ var ahsan='30';
+ var jun='6';
+
+var ahsan=parseInt(ahsan);
+var jun=parseInt(jun);
+console.log(ahsan+jun);
+
+
