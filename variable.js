c1d037b6c09b17ccbc6ee83f22cd4cf0c456537c
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-03-27 14:42:49 +0600

    variable And Data Type

---
+++ variable.js
@@ -0,0 +1,26 @@
+//Vary+able=variable
+
+//1.let
+//2.const
+//3.var
+//4.let
+//5.let
+//6.let
+//7.let
+
+
+//numeric
+let age = 23;
+let marks = 34.56;
+let rollno = 23;
+
+
+//String
+var name = 'Ahsan';
+var name = 'Rizwan';
+var name = 'Shahzad Hossain';
+
+//Boolean
+let isMarried = false;
+let isOld = true;
+var isActive = true;
