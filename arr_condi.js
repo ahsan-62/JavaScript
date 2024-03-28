c22e1d42bd36705860c28e3c03f554189e2766eb
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-03-28 13:48:11 +0600

    find Index

--- arr_condi.js
+++ arr_condi.js
@@ -1,5 +1,21 @@
+//Array in Javascripts
+
 var age=[30,90,50,44,32,34,67];
 var name=['Ahsan','Jun','Shahzad','Hossain','Rizwan','Shahzad','Hossain'];
 
 console.log(age);
 console.log(name);
+
+//get elements by Index
+
+var element=age[2];
+console.log(element);
+
+
+//set element by Index
+age[1]=60;
+console.log(age[1]);
+
+//Find Index
+
+console.log(age.indexOf(34));
