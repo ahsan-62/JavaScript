a3f602d3b4a2df23553e4ce1243595a69072fa93
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-03-29 23:41:41 +0600

    while loop

---
+++ loop.js
@@ -0,0 +1,13 @@
+
+var age=0;
+while(age<10){
+    console.log("Age is Under 10");
+    age++;
+}
+console.log("break ")
+
+var age=10;
+while(age>0){
+    console.log("Age is Under 10");
+    age--;
+}
