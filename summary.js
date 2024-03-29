8a6ebd39272bbf1a4760b1c8bbfebaf87bcf0c90
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-03-29 17:23:07 +0600

    summary of module

---
+++ summary.js
@@ -0,0 +1,11 @@
+var des=['Sajek','saint-martin','sreemangal'];
+console.log(des);
+
+console.log(des.length);
+console.log(des[2]);
+console.log(des.indexOf('saint-martin'));
+
+des.push('sherpur');
+console.log(des);
+des.pop();
+console.log(des);
