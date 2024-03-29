7bb12b50c1e7f62888a801dd00b8e8bd37ba652f
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-03-29 12:24:25 +0600

    problem 2 completed

---
+++ problem2.js
@@ -0,0 +1,16 @@
+var a =9;
+var b=8;
+var c=9;
+
+if(a==b){
+    console.log('a & b are equal');
+}
+else if(b==c){
+    console.log('b & c are equal');
+}
+else if(a==c){
+    console.log('a & c are equal');
+}
+else{
+    console.log('Not isosceles');
+}
