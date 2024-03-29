f999092efb629eb674eb30a1dce2e28f79857eeb
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-03-29 23:58:50 +0600

    odd even number by while and conditions

--- loop.js
+++ loop.js
@@ -1,13 +1,57 @@
 
-var age=0;
-while(age<10){
-    console.log("Age is Under 10");
-    age++;
-}
-console.log("break ")
 
-var age=10;
-while(age>0){
-    console.log("Age is Under 10");
-    age--;
+//While Loop Example in JS
+// var age=0;
+// while(age<10){
+//     console.log("Age is Under 10");
+//     age++;
+// }
+// console.log("break ")
+
+// var age=10;
+// while(age>0){
+//     console.log("Age is Under 10");
+//     age--;
+// }
+
+//Print Number from 1 to 10 using while loop
+
+// var num=0;
+// while(num<=10){
+//     console.log(num);
+//     num++;
+// }
+
+console.log(".................................");
+//Print odd Number from 1 to 20 using while loop
+
+// var num=1;
+// while(num<=10){
+//     console.log(num);
+//     num=num+2;
+// }
+
+
+var num=0;
+while(num<=10){
+    if(num%2!=0){
+        console.log(num);
+    }
+    num++;
 }
+console.log(".................................");
+
+// var num=0;
+// while(num<=10){
+//     console.log(num);
+//     num=num+2;
+// }
+
+var num=0;
+while(num<=10){
+    if(num%2!=1){
+        console.log(num);
+    }
+    num++;
+}
+
