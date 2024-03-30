638a6a7454b5179b044c8c366464eeb65ceb984a
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-03-30 13:06:27 +0600

    function with multiple parameters and return value

--- fun_obj.js
+++ fun_obj.js
@@ -2,23 +2,34 @@
 
 //Function
 
-function add(a,b){
+// function add(a,b){
 
-    var c=a+b;
-    console.log(c);
+//     var c=a+b;
+//     console.log(c);
+// }
+
+// add(5,7);
+
+
+
+
+// function money(money){
+//     var perPc=500;
+//     var quantities=money/perPc;
+//     return quantities;
+// }
+
+// var total=money(10000);
+
+// console.log('Total PC:',total);
+
+
+function averageMarks(num1,num2,num3){
+
+    var average=(num1+num2+num3)/3;
+    return average;
 }
 
-add(5,7);
+var total=averageMarks(70,80,90);
 
-
-
-
-function money(money){
-    var perPc=500;
-    var quantities=money/perPc;
-    return quantities;
-}
-
-var total=money(10000);
-
-console.log('Total PC:',total);
+console.log( 'Total Average:',total)
