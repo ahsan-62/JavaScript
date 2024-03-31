36883683baade71b62059aae349faa51e249ac23
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-03-31 22:36:53 +0600

    js object

--- fun_obj.js
+++ fun_obj.js
@@ -2,37 +2,37 @@
 
 //Function
 
-function add(a,b){
+function add(a, b) {
 
-    var c=a+b;
+    var c = a + b;
     console.log(c);
 }
 
-add(5,7);
+add(5, 7);
 
 
 
 
-function money(money){
-    var perPc=500;
-    var quantities=money/perPc;
+function money(money) {
+    var perPc = 500;
+    var quantities = money / perPc;
     return quantities;
 }
 
-var total=money(10000);
+var total = money(10000);
 
-console.log('Total PC:',total);
+console.log('Total PC:', total);
 
 
-function averageMarks(num1,num2,num3){
+function averageMarks(num1, num2, num3) {
 
-    var average=(num1+num2+num3)/3;
+    var average = (num1 + num2 + num3) / 3;
     return average;
 }
 
-var total=averageMarks(70,80,90);
+var total = averageMarks(70, 80, 90);
 
-console.log( 'Total Average:',total)
+console.log('Total Average:', total)
 
 
 /*
@@ -47,31 +47,30 @@ console.log( 'Total Average:',total)
 
 //Object in JS
 
-var ahsan={
-    name:'ahsan',
-    age:0,
-    height:6.2
+var ahsan = {
+    name: 'ahsan',
+    age: 0,
+    height: 6.2
 }
 console.log(ahsan);
-ahsan.name='jun'; //inject a new property/key-value to ahsan object
+ahsan.name = 'jun'; //inject a new property/key-value to ahsan object
 console.log(ahsan);
 
 
 //Multiple Object set and get
 
 
-var jun={
-    name:"JUN",
-    age:25,
-    id:180119,
-    reg:101792
+var jun = {
+    name: "JUN",
+    age: 25,
+    id: 180119,
+    reg: 101792
 }
 
 console.log(jun);
 
 console.log(jun.name);
-var name=jun['name'];
-name='ahsan';
+var name = jun['name'];
+name = 'ahsan';
 console.log(name);
 
-name
