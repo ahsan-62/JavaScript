c5a754ad067ab42c078edb042d31509da96f4f7e
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-04-02 00:29:52 +0600

    summary

--- summary.js
+++ summary.js
@@ -1,4 +1,16 @@
-var des=['Sajek','saint-martin','sreemangal'];
+/*
+1. Array
+2. Object
+3. Function
+4. Loop
+5. Conditional
+6. Operator
+7. Shorthand
+*/
+
+//Array
+
+/*var des=['Sajek','saint-martin','sreemangal'];
 console.log(des);
 
 console.log(des.length);
@@ -9,3 +21,72 @@ des.push('sherpur');
 console.log(des);
 des.pop();
 console.log(des);
+*/
+
+//Conditional-If-Else
+
+var color='red';
+
+if(color=='blue'){
+    console.log('color is blue');
+}
+else if(color=='red'){
+    console.log('color is red');
+}
+else if(color=='green'){
+    console.log('color is green');
+}
+else{
+    console.log('color is not blue or red');
+}
+
+
+//Switch-Case
+
+switch(color){
+    case('blue'):
+    console.log('color is blue');
+    break;
+    case('red'):
+    console.log('color is red');
+    break;
+    case('green'):
+    console.log('color is green');
+    break;
+    default:
+    console.log('color is not blue or red');
+}
+
+//Function
+
+function ahsan(a,b){
+    var c=a+b;
+    return c;
+}
+console.log(ahsan(5,6));
+
+function average(a,b,c){
+    var avrg=(a+b+c)/3;
+    return avrg;
+}
+
+var total=average(70,80,90);
+var total1=average(50,60,70);
+console.log('Total Average:',total,'Second Total Average:',total1);
+
+//Object
+var ahsan={
+    name:'ahsan',
+    age:25,
+    id:180119,
+    reg:101792
+}
+
+console.log(ahsan);
+ahsan.age=87;
+console.log(ahsan);
+ahsan['age']=76;
+console.log(ahsan);
+
+
+console.log(ahsan.age);
